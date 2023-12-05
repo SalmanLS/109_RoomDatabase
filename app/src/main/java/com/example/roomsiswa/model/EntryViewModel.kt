046a49a7a1c78c3878ed338105f107793cc30264
SalmanLS <salmanluthfi456@gmail.com>
2023-12-05 19:05:18 +0700

    add function input validation

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -13,5 +13,15 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    /** Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState (detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
 }
 
