9e5d3c8a2f3178bd726c06d577074448d9edc4a7
SalmanLS <salmanluthfi456@gmail.com>
2023-12-05 19:04:10 +0700

    add function recent mahasiswa status

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.roomsiswa.model
 
-class EntryViewModel{
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    /**
+     * Berisi status siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
 }
 
